c558d57d36d48dcffc74d275c0b58b515a0a43a7
anuragmis2022-del <anuragmis2022@gmail.com>
2026-06-07 22:40:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 function volume_sphere() {
     //Write your code here
-  
+  const radius=document.getElementById("radius")
+	 const volume=document.getElementById("volume")
+	const btn=document.getElementById("submit")
+
+btn.addEventListener("click", ()=>{
+
+	const radval=radius.value
+	let vol= "NaN"
+
+	if(radval>1)
+	{
+		vol= (4 / 3) * Math.PI * r ** 3;
+	}
+
+	
+})
+
+	volume.append(vol)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
